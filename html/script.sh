8b3a18ec8a0cf841ea384bedccd85c0bcb71ebde
Sanjee Pandey <sanjeevpandey1988@gmail.com>
2018-07-06 00:06:16 +0530

    added script file and deleted module folder

---
+++ html/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo rm -rf /etc/httpd/conf.d/*
+#sudo rm -rf /usr/share/nginx/html/index.html
+sudo cp -R * /usr/share/nginx/html/
+#sudo chown -R nginx:nginx /usr/share/nginx/html/
+sudo nginx -t 
+sudo service nginx restart
