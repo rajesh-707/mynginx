325678845864d5747938794934120d1d1f3bd6f8
Sanjee Pandey <sanjeevpandey1988@gmail.com>
2018-07-06 00:33:19 +0530

    removing unwanted content from sctip.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo rm -rf /etc/httpd/conf.d/*
-#sudo rm -rf /usr/share/nginx/html/index.html
 sudo cp -R html/* /usr/share/nginx/html/
-#sudo chown -R nginx:nginx /usr/share/nginx/html/
 sudo nginx -t 
 sudo service nginx restart
