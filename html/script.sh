7647f6ac325d732dbf353a73a4ebd7b9efb904e9
Sanjee Pandey <sanjeevpandey1988@gmail.com>
2018-07-06 00:29:17 +0530

    changed the color

--- html/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo rm -rf /etc/httpd/conf.d/*
-#sudo rm -rf /usr/share/nginx/html/index.html
-sudo cp -R * /usr/share/nginx/html/
-#sudo chown -R nginx:nginx /usr/share/nginx/html/
-sudo nginx -t 
-sudo service nginx restart
